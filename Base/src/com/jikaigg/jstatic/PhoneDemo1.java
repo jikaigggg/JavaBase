a1e5445a269204d04ca3bd1f34137d2a9d0b3eb7
jikaigg <jikaigg@gmail.com>
2019-10-31 15:20:48 +0800

    2019103111

---
+++ Base/src/com/jikaigg/jstatic/PhoneDemo1.java
@@ -0,0 +1,10 @@
+package com.jikaigg.jstatic;
+
+public class PhoneDemo1 {
+    public static void main(String[] args) {
+        /*Phone p = new Phone("red",3,true);
+        System.out.println("颜色："+p.getColor()+"    机龄："+p.getAge()+"    是否为智能机："+p.isSmart());*/
+        Phone p = new Phone();
+
+    }
+}
