7044709f75e55930661ff100ccc7a9ecc867a799
jikaigg <jikaigg@gmail.com>
2019-10-31 18:23:29 +0800

    201910311111

--- Base/src/com/jikaigg/jstatic/PhoneDemo1.java
+++ Base/src/com/jikaigg/jstatic/PhoneDemo1.java
@@ -5,6 +5,11 @@ public class PhoneDemo1 {
         /*Phone p = new Phone("red",3,true);
         System.out.println("颜色："+p.getColor()+"    机龄："+p.getAge()+"    是否为智能机："+p.isSmart());*/
         Phone p = new Phone();
+        System.out.println(getMax(3, 1));
 
+    }
+
+    public static int getMax(int a, int b) {
+        return a > b ? a : b;
     }
 }
