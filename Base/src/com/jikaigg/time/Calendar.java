52e34c85c92da547a9fc4c4c7b30d9c90d0764a7
jikaigg <jikaigg@gmail.com>
2019-11-05 18:33:59 +0800

    20191105111

--- Base/src/com/jikaigg/time/Calendar.java
+++ Base/src/com/jikaigg/time/Calendar.java
@@ -1,5 +1,6 @@
 package com.jikaigg.time;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 /*
@@ -8,14 +9,38 @@ import java.time.LocalDate;
  * */
 public class Calendar {
     public static void main(String[] args) {
+        // 获取当前时间
         LocalDate date = LocalDate.now();
-
+        // 获取当前月份
         int month = date.getMonthValue();
+        // 获取当前在本月中的第几天
         int day = date.getDayOfMonth();
-        //date = date.minusDays(da);
-        System.out.println(date);
-
-
+        date = date.minusDays(day - 1);
+        // 获取今天是星期几
+        DayOfWeek weekday = date.getDayOfWeek();
+        //获取今天是本周中的第几天
+        int value = weekday.getValue();
+        int valuef = value;
+        // 测试输出
+        System.out.println(month + "," + day + "," + date + "," + weekday + "," + value);
+        // 打印输出表头
         System.out.println("Mon Tus Wed Thu Fri Sat Sun");
+        // 将本月号前的空格打印输出
+        for (int i = 1; i < value; i++) {
+            System.out.print("    ");
+        }
+        // 打印
+        for (int i = 1; i < date.lengthOfMonth() + 1; i++) {
+            if (value % 7 == 0 && value < valuef + 9) {
+                System.out.println("   " + i);
+            } else if (value % 7 == 0) {
+                System.out.println("  " + i);
+            } else if (value < valuef + 9) {
+                System.out.print("   " + i);
+            } else {
+                System.out.print("  " + i);
+            }
+            value++;
+        }
     }
 }
