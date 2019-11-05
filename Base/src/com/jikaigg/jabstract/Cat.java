d1c580df6e6de58a21b2faedc4f1ae875873a7e3
crayonjk <jikaigg@outlook.com>
2019-11-06 00:29:31 +0800

    201911052

---
+++ Base/src/com/jikaigg/jabstract/Cat.java
@@ -0,0 +1,7 @@
+package com.jikaigg.jabstract;
+
+public class Cat extends Animal {
+    public void eat(){
+        System.out.println("猫吃鱼！");
+    }
+}
--- Base/src/com/jikaigg/jextends/ExtrendDemo2.java
+++ Base/src/com/jikaigg/jextends/ExtrendDemo2.java
@@ -1,8 +1,11 @@
 package com.jikaigg.jextends;
 
-/*
- *
- *
- * */
+
+
 public class ExtrendDemo2 {
+    public static void main(String[] args) {
+        Zi z = new Zi();
+        z.show();
+        z.method();
+    }
 }
