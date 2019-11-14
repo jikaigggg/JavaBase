f77e3698089d91defe845f572f24373c31290d35
crayonjk <jikaigg@outlook.com>
2019-11-15 00:14:55 +0800

    201911142

---
+++ Base/src/com/jikaigg/base/demo/EqualsDemo.java
@@ -0,0 +1,13 @@
+package com.jikaigg.base.demo;
+
+public class EqualsDemo {
+    public static void main(String[] args) {
+        String str1 = new String("yaojikai");
+        String str2 = new String("yaojikai");
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+
+
+    }
+
+}
