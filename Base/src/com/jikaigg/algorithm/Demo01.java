a1df8932c3506814db7683175a20ce8f6bef68bf
jikaigg <jikaigg@gmail.com>
2019-11-21 14:54:33 +0800

    20191121111

--- Base/src/com/jikaigg/algorithm/Demo01.java
+++ Base/src/com/jikaigg/algorithm/Demo01.java
@@ -7,21 +7,33 @@ import java.util.ArrayList;
 */
 public class Demo01 {
     public static void main(String[] args) {
+        // 创建一个ArrayList用来存放素数
         ArrayList<Integer> arr = new ArrayList<>();
+        // 计数器
         int count = 0;
+        // 双层循环嵌套
+        // 外层循环101-200之间的数
         for (int i = 101; i < 201; i++) {
-            for (int j = 3; j < i; j++) {
-                if(i%j==0){
-                    break;
-                }else {
-                    arr.add(i);
-                    count++;
-                    break;
+            // 定义一个flag标记判断情况
+            boolean flag = true;
+            // 内层循环循环3 到 该数本身-1的数
+            for (int j = 3; j < (i/2); j++) {
+                // 判断外循环对内循环取余，如果 == 0 则证明能整除，非素数
+                if (i % j == 0) {
+                    // 如果能被整出，flag标记为false
+                    flag = false;
                 }
+            }
+            // 统计过完循环flag仍为true
+            if(flag ==true){
+                // 计数器+1
+                count++;
+                // 将i添加进ArrayList中
+                arr.add(i);
             }
         }
         System.out.println(count);
-        System.out.println(arr+"3");
+        System.out.println(arr);
 
 
     }
