456a1154b9f2ece145b0b20b06ce331980b0f7e6
jikaigg <jikaigg@gmail.com>
2019-11-25 16:45:34 +0800

    2019112511

--- Base/src/com/jikaigg/algorithm/Demo04.java
+++ Base/src/com/jikaigg/algorithm/Demo04.java
@@ -1,6 +1,25 @@
 package com.jikaigg.algorithm;
-/*
 
+/*
+    画三角形星星
 */
 public class Demo04 {
+    public static void main(String[] args) {
+        getTriangle(5);
+    }
+
+    public static void getTriangle(int x) {
+        //
+        for (int i = 0; i < x; i++) {
+            for (int k = x - i; k > 0; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i * 2; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
+
