522907f6d4b2c3cb06e69ca92ed709bab156ed87
crayonjk <jikaigg@outlook.com>
2019-11-27 00:37:27 +0800

    20191127

--- Base/src/com/jikaigg/base/io/file/FileDemo2.java
+++ Base/src/com/jikaigg/base/io/file/FileDemo2.java
@@ -1,8 +1,64 @@
 package com.jikaigg.base.io.file;
-/*
 
+import java.io.File;
+
+/*
+    File类的构造方法
 
 */
 public class FileDemo2 {
+    public static void main(String[] args) {
+        /*
+        File类的构造方法
+        */
+        method1();
+        method2();
+        method3();
+    }
 
+    /*
+    File(String pathname)通过将给定路径名字符串转换为抽象路径名来创建一个新File实例
+    参数：
+        String pathname：字符串的路径名称
+        路径可以是以文件结尾，也可以是一文件夹结尾
+        路径可以是相对路径，也可以是绝对路径
+        路径可以是存在的，也可以是不存在的
+        创建File对象，只是把字符串路径封装为File对象，不考虑路径的真假情况
+     */
+    private static void method1() {
+        // 返回路径
+        File f1 = new File("/home/yaojikai/git");
+        System.out.println(f1);
+        // 相对路径
+        File f2 = new File("../FileDemo1");
+        System.out.println(f2);
+    }
+
+    /*
+    File(String parent,String child)：根据parent路径名字符串和child路径名字符串创建一个新File实例
+    参数：把路径分成了两部分
+        String parent父路径
+        String child子路径
+    好处：
+        父路径和子路径可以单独书写，使用起来非常灵活，父路径和子路径都可以变化
+    */
+    private static void method2() {
+        File f3 = new File("/home/jikaigg", "git");
+        System.out.println(f3);
+    }
+
+    /*
+    File(File parent,String child)：根据parent抽象路径名和child路径名字符串创建一个新File实例
+    参数：把路径分成了两部分
+        File parent：父路径
+        String child：子路径
+    好处：
+        父路径和子路径，可以单独书写，使用起来非常灵活；父路径和子路径都可以变化
+        父路径是File类型，可以使用File的方法对路径进行一些操作，在使用路径创建对象
+    */
+    private static void method3() {
+        File f4 = new File("/home/jikaigg");
+        File f5 = new File(f4, "git");
+        System.out.println(f5);
+    }
 }
---
+++ Base/src/com/jikaigg/base/io/file/FileDemo3.java
@@ -0,0 +1,101 @@
+package com.jikaigg.base.io.file;
+
+import java.io.File;
+
+/*
+File类的常用方法：
+    public String getAbsolutePath():返回此file的绝对路径名字符串
+    public String getPath():将此File转换为路径名字符串
+    public String getName():返回由此File表示的文件或目录的名称
+    public long Length():返回由此File还表示的文件的长度
+*/
+public class FileDemo3 {
+    public static void main(String[] args) {
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+
+    /*
+        public String getAbsolutePath():返回此file的绝对路径名字符串
+        获取的构造方法中传递的路径
+        无论路径是绝对的还是相对的，getAbsolutePath()返回的都是绝对路径
+     */
+    private static void method1() {
+        // 传绝对路径
+        File f1 = new File("/home/jikaigg/git/JavaBase/README.md");
+        String absolutePath1 = f1.getAbsolutePath();
+        System.out.println(absolutePath1);
+        // 传相对路径
+        File f2 = new File("README.md");
+        String absolutePath2 = f2.getAbsolutePath();
+        System.out.println(absolutePath2);
+    }
+
+    /*
+        public String getPath():将此File转换为路径名字符串
+
+        toString()调用的就是getPath方法
+     */
+    private static void method2() {
+        // 将File转换为字符串
+        File f3 = new File("/home/jikaigg/git/JavaBase/README.md");
+        String path1 = f3.getPath();
+        System.out.println(path1);
+        // toString()源码调用的就是getPath()方法
+        System.out.println(f3.toString());
+
+        // 将File转换为字符串
+        File f4 = new File("README.md");
+        String path2 = f4.getPath();
+        System.out.println(path2);
+    }
+
+    /*
+        public String getName():返回由此File表示的文件或目录的名称
+        获取的就是构造方法传递路径的结尾部分（文件/文件夹）
+     */
+    private static void method3() {
+        // 返回文件的名字，下面例子返回README.md
+        File f5 = new File("/home/jikaigg/git/JavaBase/README.md");
+        String name1 = f5.getName();
+        System.out.println(name1);
+
+        // 返回目录的名字，下面例子返回JavaBase
+        File f6 = new File("/home/jikaigg/git/JavaBase");
+        String name2 = f6.getName();
+        System.out.println(name2);
+    }
+
+    /*
+        public long Length():返回由此File还表示的文件的长度
+        获取的是构造方法指向的文件的大小，以字节为单位
+        注意：
+            文件夹没有大小概念，不能获取文件夹的大小
+            如果构造方法中给出的路径不存在，那么Length方法返回0
+     */
+    private static void method4() {
+        // 绝对路径获取文件的大小，下面例子返回了10
+        File f7 = new File("/home/jikaigg/git/JavaBase/README.md");
+        long length1 = f7.length();
+        System.out.println(length1);
+
+        // 相对路径获取文件的大小 下面例子返回了10
+        File f8 = new File("README.md");
+        long length2 = f8.length();
+        System.out.println(length2);
+
+        // 文件是一个目录，返回值不确定，Linux下返回了4096，有的地方说会返回0
+        File f9 = new File("/home/jikaigg/git/JavaBase");
+        long length3 = f9.length();
+        System.out.println(length3);
+
+        // 文件不存在，返回0
+        File f10 = new File("/home/jikaigg/git/JavaBase/yaojikai.txt");
+        long length4 = f10.length();
+        System.out.println(length4);
+
+
+    }
+}
