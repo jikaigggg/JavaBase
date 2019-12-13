ff01f9f8962f396780c4523bafe29ff62a588729
jikaigg <jikaigg@gmail.com>
2019-12-13 18:42:30 +0800

    3150040907

---
+++ Base/src/com/jikaigg/algorithm/RedBlackDemo.java
@@ -0,0 +1,19 @@
+package com.jikaigg.algorithm;
+
+public class RedBlackDemo {
+    private final int R = 0;
+    private final int B = 1;
+
+    private Node root = null;  // 根节点
+    class Node{
+        int data;   // 存的具体数字
+        int color = R;
+        Node left;
+        Node right;
+        Node parent;
+    }
+    public void insert(Node root,int data){
+
+    }
+
+}
