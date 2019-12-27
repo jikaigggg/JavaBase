116a870546acee868cec6f098a0045408b2e8552
jikaigg <jikaigg@gmail.com>
2019-12-27 17:50:47 +0800

    201912271111

--- Base/src/com/jikaigg/demo/demo1.java
+++ Base/src/com/jikaigg/demo/demo1.java
@@ -2,7 +2,8 @@ package com.jikaigg.demo;
 
 public class demo1 {
     public static void main(String[] args) {
-        Double 么济恺 = 3.14;
-        System.out.println(么济恺);
+        double a = 0.03*0.15;
+        double b = 0.02*0.85;
+        System.out.println(a*b);
     }
 }
