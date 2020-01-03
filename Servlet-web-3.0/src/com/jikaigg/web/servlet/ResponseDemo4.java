f6d196dc1048e4164162cc7f7148c49ebd0c82db
jikaigg <jikaigg@gmail.com>
2020-01-03 18:36:39 +0800

    20190103111

---
+++ Servlet-web-3.0/src/com/jikaigg/web/servlet/ResponseDemo4.java
@@ -0,0 +1,26 @@
+package com.jikaigg.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/response3")
+public class ResponseDemo4 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1.获取字符输出流
+        PrintWriter pw = resp.getWriter();
+        // 2.输出数据
+        // pw.write("<h1>hello response</h1>");
+        pw.write("<h1>你好 response</h1>");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+}
